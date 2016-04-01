406dd9146e77bfabfb89b511592e6973608c51e7
kbrannan <brannan.kevin@deq.state.or.us>
2016-03-31 17:01:47 -0700

    trying to get the names of the parts of the obj function

--- r-files/compare-obj-fun-results.R
+++ r-files/compare-obj-fun-results.R
@@ -3,11 +3,22 @@ chr.pest.rec.dir <- "M:/Models/Bacteria/HSPF/Big-Elk-Cadmus-HydCal-Updated-WDM"
 chr.org.rec <- scan(file = paste0(chr.pest.rec.dir, 
                       "/org-calib/Final_Deliverables_EPA_July2012/PEST_end/control.rec"),
                     sep = "\n", what = "character")
+
+
+
+
+
 tmp.top <- 
   grep("See file .*\\.seo for composite observation sensitivities\\.", 
        chr.org.rec) + 1
 
 tmp.bot <- grep("Correlation Coefficient ----->", chr.org.rec) - 1
+
+chr.org.rec.phi.blk <- chr.org.rec[tmp.top:tmp.bot]
+chr.org.rec.phi.blk <- chr.org.rec.phi.blk[-1 * grep("Objective function ----->", chr.org.rec.phi.blk)]
+
+gsub('', "", chr.org.rec.phi.blk) ## get the names of the part of the oj function
+
 
 phi.tot.org <- as.numeric(
   gsub("Sum of squared weighted residuals \\(ie phi\\)                \\=","",
