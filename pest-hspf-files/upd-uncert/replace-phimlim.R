9d64dfb1a5bf75672d98d33ae6b7220ac7b37ed7
kbrannan <brannan.kevin@deq.state.or.us>
2016-04-15 12:24:41 -0700

    changed path name

---
+++ pest-hspf-files/upd-uncert/replace-phimlim.R
@@ -0,0 +1,17 @@
+chr.uncert.dir <- "c:/temp/uncert-upd"
+
+
+chr.addreg <- list.files(path = paste0(chr.uncert.dir, "/addreg-files"), 
+                                       pattern = "^add.*\\.pst")
+
+for(ii in 1:length(chr.addreg)) {
+  addrg <- file(paste0(chr.uncert.dir, "/addreg-files/", chr.addreg[ii]))
+  chr.tmp <- readLines(addrg)
+  close(addrg)
+  chr.tmp[13840] <- "7.0000000E+03  1.0500000E-10  0.1000000"
+  pst <- file(paste0(chr.uncert.dir, "/uncert-pst/", "uncert", ii, ".pst"), "w")
+  writeLines(chr.tmp, con = pst)
+  close(pst)
+  print(paste0(" uncert", ii, ".pst compltetd"))
+  rm(chr.tmp)
+}
