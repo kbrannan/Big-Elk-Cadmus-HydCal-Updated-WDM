dd4ab6363dfee9019bb8a4b5df78f1a5a3b816cb
kbrannan <brannan.kevin@deq.state.or.us>
2016-06-13 15:54:45 -0700

    updated paths for current runs

--- org-calib/Final_Deliverables_EPA_July2012/PEST_end/model.bat
+++ org-calib/Final_Deliverables_EPA_July2012/PEST_end/model.bat
@@ -1,4 +1,4 @@
 @echo off
-C:\PEST\par2par p2p_bigelk.dat > nul
-C:\BASINS\models\HSPF\bin\WinHspfLt.exe -1 -1 C:\HSPF\PEST\bigelk.uci
-C:\PEST\swutils\tsproc < tsproc.in > nul
+E:\PEST\par2par p2p_bigelk.dat > nul
+C:\BASINS41\models\HSPF\bin\WinHspfLt.exe -1 -1 M:\Models\Bacteria\HSPF\Big-Elk-Cadmus-HydCal-Updated-WDM\org-calib\Final_Deliverables_EPA_July2012\PEST_end\bigelk.uci
+E:\PEST\surface_water_utilities\tsproc < tsproc.in > nul
