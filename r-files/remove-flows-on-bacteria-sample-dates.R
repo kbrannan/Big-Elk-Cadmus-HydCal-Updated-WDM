9ad387fdc301e7779f2ae84e9d0273b156ed7137
kbrannan <brannan.kevin@deq.state.or.us>
2016-06-15 16:14:33 -0700

    got as fair as getting rows in the flow data.frame for the dates the bacteria samples collected

---
+++ r-files/remove-flows-on-bacteria-sample-dates.R
@@ -0,0 +1,28 @@
+# get obs flow from Yaquina River gage
+source(file = "//deqhq1/tmdl/TMDL_WR/MidCoast/Models/Bacteria/HSPF/HydroCal201506/R_projs/Select_Storm_HydCal/devel/get-obs-flow-data.R")
+
+# estimate flow for Big Elk Creek from Yaquina River Gage
+source(file = "//deqhq1/tmdl/TMDL_WR/MidCoast/Models/Bacteria/HSPF/HydroCal201506/R_projs/Select_Storm_HydCal/devel/estimate-flow.R")
+
+## primary path
+chr.dir.prime <- "M:/Models/Bacteria/HSPF/Big-Elk-Cadmus-HydCal-Updated-WDM"
+
+## bacteria data path
+chr.dir.bac.obs <- paste0(chr.dir.prime, "/ObsData")
+
+## obs bacteria data file
+chr.file.obs.bac <- "obs.RData"
+
+## load obs bacteria data
+load(file = paste0(chr.dir.bac.obs, "/", chr.file.obs.bac))
+
+## get all dates
+chr.dates.bac.all <-  strftime(obs.data$date, format = "%Y-%m-%d")
+
+## remove duplicated dates
+chr.dates.bac.unique <- unique(chr.dates.bac.all)
+chr.dates.bac.unique <- chr.dates.bac.unique[order(chr.dates.bac.unique)]
+
+## find rows in flow data.frame for dates of bacteria samples
+lng.bac.flow.rows <- grep(pattern = paste0(chr.dates.bac.unique, collapse = "|"),
+                          strftime(df.flow.est$date, format = "%Y-%m-%d"))
