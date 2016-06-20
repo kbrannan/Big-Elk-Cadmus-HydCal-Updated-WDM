ec33eeca0015a34174b666820c419dcc187dc1ec
kbrannan <brannan.kevin@deq.state.or.us>
2016-06-20 16:03:44 -0700

    a little clean up

--- r-files/proc-obs-flow-for-pest.R
+++ r-files/proc-obs-flow-for-pest.R
@@ -26,75 +26,67 @@ da.be <- 88.8
 
 df.chk <- data.frame(fwd = rep(-1000, length(lng.bac.flow.rows)),
                       bck = -1000)
-df.chk$fwd[1] <- lng.bac.flow.rows[2] - lng.bac.flow.rows[1]
+df.chk$fwd[1] <- lng.bac.flow.rows[1] - 1
 for(ii in 2:(length(lng.bac.flow.rows) - 1)) {
   df.chk$fwd[ii] <- lng.bac.flow.rows[ii + 1] - lng.bac.flow.rows[ii]
   df.chk$bck[ii] <- lng.bac.flow.rows[ii] - lng.bac.flow.rows[ii - 1]
 }
-df.chk$bck[length(lng.bac.flow.rows)] <- 
-  lng.bac.flow.rows[length(lng.bac.flow.rows)] - 
-  lng.bac.flow.rows[length(lng.bac.flow.rows) - 1]
+rm(ii)
+df.chk$bck[length(lng.bac.flow.rows)] <- length(df.flow.est$date) -
+  lng.bac.flow.rows[length(lng.bac.flow.rows)]
 df.chk <- cbind(df.chk, chk = 0)
-
 df.chk$chk[df.chk$fwd > 1 & df.chk$bck > 1] <- 1
 if(df.chk$fwd[1] > 1) df.chk$chk[1] <- 1
 if(df.chk$bck[length(lng.bac.flow.rows)] > 1) df.chk$chk[length(lng.bac.flow.rows)] <- 1
 
 
-lng.chk[length(lng.chk)] <- lng.bac.flow.rows[length(lng.chk)] - 
-  lng.bac.flow.rows[length(lng.chk) - 1]
-rm(ii)
 
-df.bnds <- data.frame(start = rep(-1, length(lng.bac.flow.rows)),
-                      end = -1, lng.bac.flow.rows, lng.chk,
-                      sec.chk = -1000)
-if(lng.chk[1] > 1) {
+
+df.bnds <- cbind(start = -1,
+                 end = -1, 
+                 bac.rows = lng.bac.flow.rows,
+                 df.chk,
+                 len = -1,
+                 bfi = -1)
+if(df.bnds$chk[1] == 1) {
   df.bnds$start[1] <- 1
   df.bnds$end[1] <- lng.bac.flow.rows[1] - 1
 }
-for(ii in 2:(length(lng.bac.flow.rows) - 1)) {
-  if(lng.chk[ii] > 1) {
-    df.bnds$start[ii] <- lng.bac.flow.rows[ii - 1] + 1
-    df.bnds$end[ii] <- lng.bac.flow.rows[ii] - 1
+for(ii in 2:(length(df.bnds$bac.rows) - 1)) {
+  if(df.bnds$chk[ii] == 1) {
+    df.bnds$start[ii] <- df.bnds$bac.rows[ii - 1] + 1
+    df.bnds$end[ii] <- df.bnds$bac.rows[ii] - 1
   }
 }
 
-if(lng.chk[length(lng.chk)] > 1) {
-  df.bnds$start[length(lng.bac.flow.rows)] <- lng.bac.flow.rows[length(lng.bac.flow.rows) - 1] - 1
+if(df.bnds$chk[length(df.bnds$chk)] == 1) {
+  df.bnds$start[length(lng.bac.flow.rows)] <- lng.bac.flow.rows[length(lng.bac.flow.rows)] + 1
   df.bnds$end[length(lng.bac.flow.rows)] <- length(df.flow.est$date)
 }
 
-df.bnds$sec.chk <- df.bnds$end - df.bnds$start
+df.bnds$len <- df.bnds$end - df.bnds$start
 
-
-df.bnds.chk <- df.bnds[lng.chk > 1, ]
-
-df.bnds.chk <- cbind(df.bnds.chk, bfi = -1)
-
-for(ii in 1:length(df.bnds.chk$bfi)) {
-  tmp.seq <- seq.int(from = df.bnds.chk$start[ii], to = df.bnds.chk$end[ii])
-  tmp.hysep88.8 <- hysep(Flow = df.flow.est$mean_daily_flow_cfs[tmp.seq], 
-                         Dates = as.Date(df.flow.est$date[tmp.seq]), da = da.be)
-  df.bnds.chk$bfi[ii] <- sum(tmp.hysep88.8$BaseQ) / sum(tmp.hysep88.8$Flow)
-  rm(tmp.seq, tmp.hysep88.8)
+for(ii in 1:length(df.bnds$bfi)) {
+  if(df.bnds$chk[ii] == 1) {
+    tmp.seq <- seq.int(from = df.bnds$start[ii], to = df.bnds$end[ii])
+    tmp.hysep88.8 <- try(hysep(Flow = df.flow.est$mean_daily_flow_cfs[tmp.seq], 
+                               Dates = as.Date(df.flow.est$date[tmp.seq]), da = da.be,
+                               select = "sliding"), silent = TRUE)
+    if(class(tmp.hysep88.8)[1] == "baseflow") {
+      df.bnds$bfi[ii] <- sum(tmp.hysep88.8$BaseQ) / sum(tmp.hysep88.8$Flow)
+    }
+    rm(tmp.seq, tmp.hysep88.8)
+  }
 }
-tmp.seq <- seq.int(from = df.bnds$start[ii], to = df.bnds$end[1])
-tmp.hysep88.8 <- hysep(Flow = df.flow.est.reduced$mean_daily_flow_cfs[tmp.seq], 
-                      Dates = as.Date(df.flow.est.reduced$date[tmp.seq]), da = da.be)
-df.bnds$bfi[ii] <- sum(tmp.hysep88.8$BaseQ) / sum(tmp.hysep88.8$Flow)
 
-str(df.hysep88.8)
-
-df.hysep88.8 <- hysep(Flow = df.flow.est.reduced$mean_daily_flow_cfs, 
-                      Dates = as.Date(df.flow.est.reduced$date), da = da.be)
+lng.bfi <- grep("[^-1]", df.bnds$bfi)
 
 ## caclulate baseflow in the same way as calculated from the observed data
-mbaseind <- median(df.bnds$bfi)
 
-boxplot((df.bnds$bfi))
+mbaseind <- sum(df.bnds$len[lng.bfi] * df.bnds$bfi[lng.bfi]) / sum(df.bnds$len[lng.bfi])
 
 ## clean up
-rm(df.hysep88.8)
+rm(ii, df.chk,lng.bfi, lng.bac.flow.rows)
 
 ## convert stream flow from cu ft / sec to ac-ft / day for use in volumes
 ## (1 cu ft / sec) * (86400 sec / day) * (1 ac-ft / 43559.9 cu ft)
