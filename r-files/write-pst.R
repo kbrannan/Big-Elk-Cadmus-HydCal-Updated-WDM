2297e1496574481579efa0a62c7f77e33ecb69b8
kbrannan <brannan.kevin@deq.state.or.us>
2016-06-28 15:38:05 -0700

    completed code to create block for obs data

--- r-files/write-pst.R
+++ r-files/write-pst.R
@@ -15,20 +15,47 @@ lng.num.obs.grp <- length(chr.obs.grp)
 
 ## get numbner of observations, 
 lng.num.obs <- sum(sapply(chr.obs.grp, 
-                      function(chr.name = NULL) length(eval(as.name(chr.name)))))
+                      function(chr.name = NULL) 
+                        length(eval(as.name(chr.name)))))
+## get the number of digits to use in format of obs names
+lng.num.obs.dgt <- max(
+  nchar(
+    max(
+      sapply(chr.obs.grp, function(chr.name = NULL) 
+        length(eval(as.name(chr.name)))))), 1)
+
+## get max number of charcters for obs name
+lng.max.nchar <- max(nchar(
+  sprintf(
+    paste0(chr.obs.grp, paste0("_%0", lng.num.obs.dgt, "i")), 0)))
 
 ## create string of obs for pest control file, make weight 1/value for obs
 ## mlog_1                 1.658966        6.027852E-02  mlog
-paste0(sprintf(paste0(chr.obs.grp[1], "_%04i"), 1), "     ",
-       sprintf("%8.4E", eval(as.name(chr.obs.grp[1]))))
+chr.obs.blk <- ""
+chr.col.spc <- "     "
 
 
-paste0(sprintf(paste0(chr.obs.grp[5], "_%04i"), 1:length(eval(as.name(chr.obs.grp[5])))), "     ",
-       sprintf("%8.4E", eval(as.name(chr.obs.grp[5]))))
+for(ii in 1:length(chr.obs.grp)) {
+  tmp.grp <- chr.obs.grp[ii]
+  tmp.data <- eval(as.name(tmp.grp))
+  tmp.blk <- ""
+  for(jj in 1:length(tmp.data)) {
+    tmp.nme <- sprintf(paste0("%-",lng.max.nchar,"s"),sprintf(paste0(tmp.grp, paste0("_%0", lng.num.obs.dgt, "i")), jj))
+    tmp.val <- sprintf("%8.4E", tmp.data[jj])
+    tmp.wtg <- sprintf("%8.4E", abs(1/tmp.data[jj]))
+    tmp.blk <- c(tmp.blk,
+                 paste0(tmp.nme, chr.col.spc, tmp.val, chr.col.spc, tmp.wtg,
+                        chr.col.spc, tmp.grp))
+    rm(tmp.nme, tmp.val, tmp.wtg)
+  }
+  tmp.blk <- tmp.blk[-1]
+  chr.obs.blk <- c(chr.obs.blk, tmp.blk)
+  rm(tmp.grp, tmp.data, tmp.blk)
+}
+chr.obs.blk <- as.data.frame(chr.obs.blk[-1])
 
-
-
-
+write.table(chr.obs.blk, file = paste0(chr.dir, "/junk.tab"),
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 # get pest control file
 chr.dir.pst <- "m:/models/bacteria/hspf/bigelkhydrocal201601/pest-files"
