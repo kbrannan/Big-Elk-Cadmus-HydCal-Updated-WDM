f36007a16749888b060fe00afbbc49da7e59908b
kbrannan <brannan.kevin@deq.state.or.us>
2016-07-01 11:27:36 -0700

    updated the path to R to latest version. Got error syaing DVstats package could not be found when I used the earlier version of R. I did not have the package in the earlier version of R

--- pest-hspf-files/model.bat
+++ pest-hspf-files/model.bat
@@ -1,4 +1,4 @@
 @echo off
-E:\PEST\par2par %~dp0p2p_bigelk.dat > nul
+C:\PEST\par2par %~dp0p2p_bigelk.dat > nul
 C:\BASINS41\models\HSPF\bin\WinHspfLt.exe -1 -1 %~dp0bigelk.uci
-E:\PEST\surface_water_utilities\tsproc < %~dp0tsproc.in > nul
+"C:\Program Files\R\R-3.1.3\bin\x64\RScript.exe" M:\Models\Bacteria\HSPF\Big-Elk-Cadmus-HydCal-Updated-WDM\r-files\proc-mod-flow-for-pest.R
