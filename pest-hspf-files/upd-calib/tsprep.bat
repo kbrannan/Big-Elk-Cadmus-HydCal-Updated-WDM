9c640cbf0d8f27bfe6b559f707e28e1f630b411d
kbrannan <brannan.kevin@deq.state.or.us>
2016-07-19 15:53:29 -0700

    cleaning up. put previous work that I redid in zip-file archive

--- pest-hspf-files/upd-calib/tsprep.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-E:\PEST\par2par %~dp0p2p_bigelk.dat
-C:\BASINS41\models\HSPF\bin\WinHspfLt.exe -1 -1 %~dp0bigelk.uci
-E:\PEST\surface_water_utilities\tsproc < %~dp0tsprep.in
--- pest-hspf-files/upd-uncert/rpl-phimlim.bat
+++
@@ -1,9 +0,0 @@
-set /a ln=1
-for /F "tokens=*" %%i in (addreg1-1.pst) do (
-if !ln! == "13840" (
-echo "  6.9000000E+03  1.0500000E-10  0.1000000    " >> junk.txt
-)
-else (
-if 
-echo %%i >> junk.txt
-echo 
