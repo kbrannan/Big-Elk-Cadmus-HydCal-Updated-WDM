31e7f9a34f8d71ad0f0c918d1be8c90882ed6639
kbrannan <brannan.kevin@deq.state.or.us>
2016-08-01 11:59:15 -0700

    not using these runs but holding onto them in local disk. may use some of the code in future runs

--- pest-hspf-files/pred-unc-pre-cal/model.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-del M:\Models\Bacteria\HSPF\Big-Elk-Cadmus-HydCal-Updated-WDM\pest-hspf-files\pred-unc-pre-cal\modflow.out
-REM del M:\Models\Bacteria\HSPF\Big-Elk-Cadmus-HydCal-Updated-WDM\pest-hspf-files\model.out
-C:\PEST\par2par M:\Models\Bacteria\HSPF\Big-Elk-Cadmus-HydCal-Updated-WDM\pest-hspf-files\pred-unc-pre-cal\p2p_bigelk.dat > nul
-C:\BASINS41\models\HSPF\bin\WinHspfLt.exe -1 -1 M:\Models\Bacteria\HSPF\Big-Elk-Cadmus-HydCal-Updated-WDM\pest-hspf-files\pred-unc-pre-cal\bigelk.uci
-REM del M:\Models\Bacteria\HSPF\Big-Elk-Cadmus-HydCal-Updated-WDM\pest-hspf-files\pred-unc-pre-cal\model.out
-REM del M:\Models\Bacteria\HSPF\Big-Elk-Cadmus-HydCal-Updated-WDM\pest-hspf-files\pred-unc-pre-cal\model.ins
-"C:\Program Files\R\R-3.1.3\bin\x64\RScript.exe" M:\Models\Bacteria\HSPF\Big-Elk-Cadmus-HydCal-Updated-WDM\r-files\proc-mod-flow-for-pest-pre-cal-pred-error.R
